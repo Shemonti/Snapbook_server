1a1ece94ad93b1b80bd5bfe550e18634def75bb5
Shemonti <shemonti33@gmail.com>
2021-07-09 19:23:36 +0600

    make it better

---
+++ models/postMessage.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const PostSchema = mongoose.Schema({
+    title:String,
+    message:String,
+    creator:String,
+    tags:[String],
+    selectedFiles:String,
+    likeCount:{
+        type:Number,
+        default:0
+    },
+    UnlikeCount:{
+        type:Number,
+        default:0
+    },
+    createdAt:{
+        type:Date,
+        default:new Date()
+    },
+});
+
+const PostMessage = mongoose.model('PostMessage',PostSchema);
+export default PostMessage;
---
+++ routes/posts.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import {getPosts,createPost,updatePost,deletePost,likePost,unlikePost} from '../controllers/posts.js';
+
+const router= express.Router();
+
+router.get('/',getPosts);
+router.post('/',createPost);
+router.patch('/:id',updatePost);
+router.delete('/:id',deletePost);
+router.patch('/:id/likePost',likePost);
+router.patch('/:id/unlikePost',unlikePost);
+
+export default router;
